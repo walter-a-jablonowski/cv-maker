e0ae8cede0f2d8f05eae7d5e6de87d61b747f72d
Walter A Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-10-04 03:32:28 +0200

    Split page and design

--- src/designs/Black-design_Walter/layout.php
+++ src/designs/Black-design_Walter/layout.php
@@ -1,172 +1,153 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $data['personal']['name'] ?> - Resume</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-  <style>
-    <?php require "designs/$design/styles.css"; ?>
-  </style>
-</head>
-<body>
-  <div class="container">
-    <div class="sidebar">
-      
-      <img src="users/<?= $user ?>/img.png" alt="<?= $data['personal']['name'] ?>" class="profile-img">
-      
-      <h1 style="font-size: 2.4vw;"><?= $data['personal']['name'] ?></h1>  <!-- fix font size so that it matches container -->
-      
-      <?php if( ! empty( $data['personal']['summary'] )): ?>
-        <p><?= $data['personal']['summary'] ?></p>
-      <?php endif; ?>
+<div class="container">
+  <div class="sidebar">
+    
+    <img src="users/<?= $user ?>/img.png" alt="<?= $data['personal']['name'] ?>" class="profile-img">
+    
+    <h1 style="font-size: 2.4vw;"><?= $data['personal']['name'] ?></h1>  <!-- fix font size so that it matches container -->
+    
+    <?php if( ! empty( $data['personal']['summary'] )): ?>
+      <p><?= $data['personal']['summary'] ?></p>
+    <?php endif; ?>
 
-      <div class="contact-info">
-        <table style="margin-left: -4px;">
-          <tr>
-            <td><i class="bi bi-envelope"></i></td>
-            <td><?= $data['personal']['email'] ?></td>
-          </tr>
-          <tr>
-            <td><i class="bi bi-globe"></i></td>
-            <td><a href="<?= $data['personal']['url'] ?>"><?= str_replace(['https://', 'http://'], '', $data['personal']['url'] ) ?></a></td>
-          </tr>
-          <tr>
-            <td style="vertical-align: top;"><i class="bi bi-geo-alt-fill"></i></td>
-            <td>
-              <?= $data['personal']['location']['address'] ?> <br>
-              <?= $data['personal']['location']['postalCode'] ?> <?= $data['personal']['location']['city'] ?>
-            </td>
-          </tr>
-        </table>
-      </div>
-
-      <?php foreach( $data['skills']['dev'] as $skill => $level): ?>
-        <div class="skill" style="margin-top: 20px;">
-          <div class="skill-name"><?= $skill ?></div>
-          <div class="skill-bar">
-            <div class="skill-level" style="width: <?= $level ?>%;"></div>
-          </div>
-        </div>
-      <?php endforeach; ?>
-
-      <div class="lang-skills" style="margin-top: 40px;">
-        <?php foreach( $data['skills']['lang'] as $language => $proficiency): ?>
-          <span class="lang-skill"><?= $language ?>: <?= $proficiency ?></span>
-        <?php endforeach; ?>
-      </div>
-        
-      <div class="new-page" style="padding-top: 20px;">
-        <table class="misc-personal">
-          <tr>
-            <td><?= $captions['born'] ?>:</td>
-            <td><?= $data['personal']['dateOfBirth'] ?></td>
-          </tr>
-          <tr>
-            <td><?= $captions['nationality'] ?>:</td>
-            <td><?= $data['personal']['nationality'] ?></td>
-          </tr>
-          <tr>
-            <td><?= $captions['maritalStatus'] ?>:</td>
-            <td><?= $data['personal']['maritalStatus'] ?></td>
-          </tr>
-        </table>
-      </div>
+    <div class="contact-info">
+      <table style="margin-left: -4px;">
+        <tr>
+          <td><i class="bi bi-envelope"></i></td>
+          <td><?= $data['personal']['email'] ?></td>
+        </tr>
+        <tr>
+          <td><i class="bi bi-globe"></i></td>
+          <td><a href="<?= $data['personal']['url'] ?>"><?= str_replace(['https://', 'http://'], '', $data['personal']['url'] ) ?></a></td>
+        </tr>
+        <tr>
+          <td style="vertical-align: top;"><i class="bi bi-geo-alt-fill"></i></td>
+          <td>
+            <?= $data['personal']['location']['address'] ?> <br>
+            <?= $data['personal']['location']['postalCode'] ?> <?= $data['personal']['location']['city'] ?>
+          </td>
+        </tr>
+      </table>
     </div>
 
-    <div class="main-content">
-
-      <?php if( ! empty( $data['personal']['logo'] )): ?>
-        <div style="text-align: center;">
-          <img src="users/<?= $user ?>/<?= $data['personal']['logo'] ?>" width="150">
+    <?php foreach( $data['skills']['dev'] as $skill => $level): ?>
+      <div class="skill" style="margin-top: 20px;">
+        <div class="skill-name"><?= $skill ?></div>
+        <div class="skill-bar">
+          <div class="skill-level" style="width: <?= $level ?>%;"></div>
         </div>
-      <?php endif; ?>
+      </div>
+    <?php endforeach; ?>
+
+    <div class="lang-skills" style="margin-top: 40px;">
+      <?php foreach( $data['skills']['lang'] as $language => $proficiency): ?>
+        <span class="lang-skill"><?= $language ?>: <?= $proficiency ?></span>
+      <?php endforeach; ?>
+    </div>
       
-      <section>
-        <h2><?= $captions['summary'] ?></h2>
-        <p><?= $data['summary'] ?></p>
-      </section>
+    <div class="new-page" style="padding-top: 20px;">
+      <table class="misc-personal">
+        <tr>
+          <td><?= $captions['born'] ?>:</td>
+          <td><?= $data['personal']['dateOfBirth'] ?></td>
+        </tr>
+        <tr>
+          <td><?= $captions['nationality'] ?>:</td>
+          <td><?= $data['personal']['nationality'] ?></td>
+        </tr>
+        <tr>
+          <td><?= $captions['maritalStatus'] ?>:</td>
+          <td><?= $data['personal']['maritalStatus'] ?></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+  <div class="main-content">
+
+    <?php if( ! empty( $data['personal']['logo'] )): ?>
+      <div style="text-align: center;">
+        <img src="users/<?= $user ?>/<?= $data['personal']['logo'] ?>" width="150">
+      </div>
+    <?php endif; ?>
+    
+    <section>
+      <h2><?= $captions['summary'] ?></h2>
+      <p><?= $data['summary'] ?></p>
+    </section>
+    
+    <section>
+      <h2><?= $captions['specialSkills'] ?></h2>
+      <p><?= $data['specialSkills'] ?></p>
+    </section>
+    
+    <section>
+      <h2><?= $captions['goals'] ?></h2>
+      <p><?= $data['goals'] ?></p>
+    </section>
+    
+    <section class="new-page">
       
-      <section>
-        <h2><?= $captions['specialSkills'] ?></h2>
-        <p><?= $data['specialSkills'] ?></p>
-      </section>
+      <h2><?= $captions['experience'] ?></h2>
       
-      <section>
-        <h2><?= $captions['goals'] ?></h2>
-        <p><?= $data['goals'] ?></p>
-      </section>
-      
-      <section class="new-page">
-        
-        <h2><?= $captions['experience'] ?></h2>
-        
-        <div class="timeline">
-          <?php foreach( $data['experience'] as $job): ?>
-            <div class="experience-item">
-              <h3>
-                <span class="date"><?= $job['from'] ?> - <?= $job['until'] ?></span>
-                &nbsp;
-                <?= $job['position'] ?>
-              </h3>
-              <?php if( ! empty( $job['organisation']) || ! empty( $job['summary'])): ?>
-                <p class="details">
-                  <?php if( ! empty( $job['organisation'] )): ?>
-                    <span class="label">Company:</span> <?= $job['organisation'] ?>
-                  <?php endif; ?>
-                  <?php if( ! empty( $job['summary']) && ! empty( $job['organisation'])): ?>
-                    <br>
-                  <?php endif; ?>
-                  <?php if( ! empty( $job['summary'] )): ?>
-                    <?= $job['summary'] ?>
-                  <?php endif; ?>
-                </p>
-              <?php endif; ?>
-            </div>
-            <?php endforeach; ?>
+      <div class="timeline">
+        <?php foreach( $data['experience'] as $job): ?>
+          <div class="experience-item">
+            <h3>
+              <span class="date"><?= $job['from'] ?> - <?= $job['until'] ?></span>
+              &nbsp;
+              <?= $job['position'] ?>
+            </h3>
+            <?php if( ! empty( $job['organisation']) || ! empty( $job['summary'])): ?>
+              <p class="details">
+                <?php if( ! empty( $job['organisation'] )): ?>
+                  <span class="label">Company:</span> <?= $job['organisation'] ?>
+                <?php endif; ?>
+                <?php if( ! empty( $job['summary']) && ! empty( $job['organisation'])): ?>
+                  <br>
+                <?php endif; ?>
+                <?php if( ! empty( $job['summary'] )): ?>
+                  <?= $job['summary'] ?>
+                <?php endif; ?>
+              </p>
+            <?php endif; ?>
           </div>
-      </section>
-        
-      <section>
-          
-        <h2><?= $captions['education'] ?></h2>
-        
-        <div class="timeline">
-          <?php foreach( $data['education'] as $edu): ?>
-            <div class="education-item">
-              <h3>
-                <span class="date"><?= $job['from'] ?> - <?= $job['until'] ?></span>
-                &nbsp;
-                <?= $edu['institution'] ?>
-              </h3>
-              <?php if( ! empty( $edu['summary']) || ! empty( $edu['degree'])): ?>
-                <p class="details">
-                  <?php if( ! empty( $edu['summary'] )): ?>
-                    <?= $edu['summary'] ?>
-                  <?php endif; ?>
-                  <?php if( ! empty( $edu['summary']) && ! empty( $edu['degree'])): ?>
-                    <br>
-                  <?php endif; ?>
-                  <?php if( ! empty( $edu['degree'] )): ?>
-                    <span class="label">Degree:</span> <?= $edu['degree'] ?>
-                  <?php endif; ?>
-                </p>
-              <?php endif; ?>
-            </div>
           <?php endforeach; ?>
         </div>
-      </section>
+    </section>
+      
+    <section>
+        
+      <h2><?= $captions['education'] ?></h2>
+      
+      <div class="timeline">
+        <?php foreach( $data['education'] as $edu): ?>
+          <div class="education-item">
+            <h3>
+              <span class="date"><?= $job['from'] ?> - <?= $job['until'] ?></span>
+              &nbsp;
+              <?= $edu['institution'] ?>
+            </h3>
+            <?php if( ! empty( $edu['summary']) || ! empty( $edu['degree'])): ?>
+              <p class="details">
+                <?php if( ! empty( $edu['summary'] )): ?>
+                  <?= $edu['summary'] ?>
+                <?php endif; ?>
+                <?php if( ! empty( $edu['summary']) && ! empty( $edu['degree'])): ?>
+                  <br>
+                <?php endif; ?>
+                <?php if( ! empty( $edu['degree'] )): ?>
+                  <span class="label">Degree:</span> <?= $edu['degree'] ?>
+                <?php endif; ?>
+              </p>
+            <?php endif; ?>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </section>
 
-      <p class="footer">
-        powered by <a href="https://github.com/walter-a-jablonowski/cv-maker" target="_blank">cv-maker</a> &copy; Walter A. Jablonowski 2024
-      </p>
-    </div>
-    
+    <p class="footer">
+      powered by <a href="https://github.com/walter-a-jablonowski/cv-maker" target="_blank">cv-maker</a> &copy; Walter A. Jablonowski 2024
+    </p>
   </div>
-  <?php if( is_file("designs/$design/controller.js")): ?>
-    <script>
-      <?php require "designs/$design/controller.js"; ?>
-    </script>
-  <?php endif; ?>
-</body>
-</html>
+  
+</div>
--- src/index.php
+++ src/index.php
@@ -6,8 +6,8 @@ use Symfony\Component\Yaml\Exception\ParseException;
 require 'vendor/autoload.php';
 
 
-// $user = 'Walter';
-$user    = 'Demo';
+$user = 'Walter';
+// $user    = 'Demo';
 $design  = $_GET['design'] ?? 'Black-design_Walter';
 $lang    = 'de';
 
@@ -20,6 +20,26 @@ $html = ob_get_clean();
 
 file_put_contents("users/$user/public/cv.html", $html);
 
-echo $html;
 
-?>
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resume</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <style>
+    <?php require "designs/$design/styles.css"; ?>
+  </style>
+</head>
+<body>
+
+  <?php echo $html; ?>
+
+  <?php if( is_file("designs/$design/controller.js")): ?>
+    <script>
+      <?php require "designs/$design/controller.js"; ?>
+    </script>
+  <?php endif; ?>
+</body>
+</html>
