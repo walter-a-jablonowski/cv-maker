8af58c0f647daf0806f026a2ee9ae1c8681dec98
Walter A. Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-10-11 05:31:23 +0200

    Debugging

--- src/lib/translation.js
+++ src/lib/translation.js
@@ -15,7 +15,11 @@ function translate( lang )
   // })
 
   elems.forEach( elem => { 
-    elem.innerHTML = data[lang][ findKey(data, elem.dataset.key) ]
+    
+    if( elem.dataset.key === 'summary')
+      console.log('debug')
+
+    elem.innerHTML = findKey( data[lang], elem.dataset.key)
   })
   
   // Data lists
@@ -27,7 +31,7 @@ function translate( lang )
 
 function translate_recurse( list, lang )
 {
-  let value = findKey( data[lang], list.dataset.list )  // TASK: func has undefined
+  let value = findKey( data[lang], list.dataset.list )  // TASK: func has undefined (currently makes no difference)
   
   list.querySelectorAll(`[data-idx]`).forEach( idx => {
     
@@ -44,24 +48,3 @@ function translate_recurse( list, lang )
   //   translate_recurse( subList, lang )
   // })
 }
-
-/*
-
-function translSetString(elem, vars, attr, lang, idx = -1)
-{
-  // Select the right list entry
-  // (TASK) improve this list handling a bit? at least we can have deeper nested lists
-
-  if( idx > -1 )
-  {
-    let list = elem.closest('[data-list]').dataset.list
-    
-    if( list in value )
-      value = value[list]
-    
-    if( Array.isArray(value) && idx < value.length)  // idx is in array = select list entry only for index lists (no captions)
-      value = value[idx]
-  }
-}
-
-*/
