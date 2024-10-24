a446e746f5b597f940a74844ac285df1ab86dc98
Walter A. Jablonowski <31986246+walter-a-jablonowski@users.noreply.github.com>
2024-10-24 19:13:59 +0200

    Added favicon

--- src/page.php
+++ src/page.php
@@ -8,6 +8,10 @@ use Symfony\Component\Yaml\Exception\ParseException;
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="manifest" href="site.webmanifest">
   <title>Resume</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <style>
